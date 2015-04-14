b535b3ec68d21c74ade75e7b8a2fbfb90919601d
paunrv <pau.norv@gmail.com>
2015-04-13 22:40:57 -0700

    MongoDB Config
    Configuracion para usar mongoDB local

--- config/connections.js
+++ config/connections.js
@@ -60,8 +60,8 @@ module.exports.connections = {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
-    user: 'mongodb',
-    password: 'Enrique91',
+    user: '',
+    password: '',
     database: 'humi'
   },
 
